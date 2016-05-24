b552cdaea50c4004ff188ca822c30824351c6464
Bart JV <bartnorsk@gmail.com>
2016-05-24 19:44:05 -0300

    Minor refactors on SumOfTwoNucleotide

--- src/com/bartnorsk/basic/SumOfTwoNucleotide.java
+++ src/com/bartnorsk/basic/SumOfTwoNucleotide.java
@@ -1,34 +1,43 @@
 package com.bartnorsk.basic;
+
 /**
+ * '+' Nucleotide Operator. Requires a leftAdd Nucleotide and a rightAdd
+ * Nucleotide
+ * <p>
+ * Arity set to two by default.
  * 
  * @author Bart JV
  *
  */
-public class SumOfTwoNucleotide extends Nucleotide{
-    
-    private Nucleotide leftAdd;
-    private Nucleotide rightAdd;
-    
-    private SumOfTwoNucleotide(){
-	super(2,"+");
+public class SumOfTwoNucleotide extends Nucleotide {
+
+    private Nucleotide leftAdd = null;
+    private Nucleotide rightAdd = null;
+
+    private SumOfTwoNucleotide() {
+	super(2, "+");
     }
-    public SumOfTwoNucleotide(Nucleotide leftAdd,Nucleotide rightAdd) {
-	super(2,"+");
+
+    public SumOfTwoNucleotide(Nucleotide leftAdd, Nucleotide rightAdd) {
+	super(2, "+");
 	this.leftAdd = leftAdd;
 	this.rightAdd = rightAdd;
     }
+
     /**
      * @see com.bartnorsk.basic.INucleotide#execute()
      */
     @Override
-    public Object execute() {	
-	return (double)leftAdd.execute()+(double)rightAdd.execute();
+    public Object execute() {
+	return (double) leftAdd.execute() + (double) rightAdd.execute();
     }
+
     /*
      * Value can not be changed
+     * 
      * @see com.bartnorsk.basic.Nucleotide#setValue(java.lang.Object)
      */
-    public void setValue(Object o){
+    public void setValue(Object o) {
 	super.setValue("+");
     }
 }
